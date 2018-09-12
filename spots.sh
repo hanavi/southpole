0e846382f1c6e969b1af550aea51d8c9279ea0b0
James Casey <hanavi@gmail.com>
2018-09-12 01:41:34 -0500

    Update spots.sh
    Unnecessary cleaning...

--- spots.sh
+++ spots.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-#curl --socks5-hostname localhost:8080 https://www.dxcluster.co.uk/cluster/search -d callsign=kc4aaa \
-#echo "================================================================================================================"
 curl -s https://www.dxcluster.co.uk/cluster/search -d callsign=kc4aaa \
   | tr -d '\n' \
-  | sed 's/<table/\n<table/g' \
-  | sed 's/\/table>/\/table>\n/g' \
+  | sed 's/<table/\n<table/g; s/\/table>/\/table>\n/g' \
   | grep "class=\"spots\"" \
-  | sed 's/\t/ /g' \
-  | sed 's/<tr>/\n<tr>/g' \
-  | sed 's/<table/\n<table/g' \
-  | sed 's/<\/table>/\n<\/table>/g' \
-  | sed 's/<tr>//g' \
-  | sed 's/<td>/\t/g' \
-  | sed 's/<\/tr>//g' \
-  | sed 's/<\/td>//g' \
+  | sed 's/\t/ /g; s/<tr>/\n<tr>/g; s/<table/\n<table/g' \
+  | sed 's/<\/table>/\n<\/table>/g; s/<tr>//g; s/<td>/\t/g' \
+  | sed 's/<\/tr>//g; s/<\/td>//g' \
   | grep -v table \
   | sed 's/^   / /g' \
-  | head -n 50 \
-  | mail -s "DX Summary" jcasey@icecube.usap.gov
-#echo "================================================================================================================"
+  | head -n 50
